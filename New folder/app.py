a93e65eb896394417846383f62644e1d84f04907
Bhanu <abhiiloves00@gmail.com>
2026-06-28 15:51:38 +0530

    Add gunicorn to requirements and update TemplateResponse syntax for Linux hosting

--- New folder/app.py
+++ New folder/app.py
@@ -83,7 +83,7 @@ class ChatRequest(BaseModel):
 
 @app.get("/", response_class=HTMLResponse)
 async def get_index(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/api/sessions")
 async def get_sessions():
